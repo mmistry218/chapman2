700386744df181db45364f4517223b96680bce48
mamistry_turner <manish.mistry@turner.com>
2015-11-23 14:52:11 -0500

    Initial Commit

---
+++ sites/all/themes/bootstrap_subtheme/templates/node--product_display.tpl.php
@@ -0,0 +1,145 @@
+<?php
+
+/**
+ * @file
+ * Default theme implementation to display a node.
+ *
+ * Available variables:
+ * - $title: the (sanitized) title of the node.
+ * - $content: An array of node items. Use render($content) to print them all,
+ *   or print a subset such as render($content['field_example']). Use
+ *   hide($content['field_example']) to temporarily suppress the printing of a
+ *   given element.
+ * - $user_picture: The node author's picture from user-picture.tpl.php.
+ * - $date: Formatted creation date. Preprocess functions can reformat it by
+ *   calling format_date() with the desired parameters on the $created variable.
+ * - $name: Themed username of node author output from theme_username().
+ * - $node_url: Direct URL of the current node.
+ * - $display_submitted: Whether submission information should be displayed.
+ * - $submitted: Submission information created from $name and $date during
+ *   template_preprocess_node().
+ * - $classes: String of classes that can be used to style contextually through
+ *   CSS. It can be manipulated through the variable $classes_array from
+ *   preprocess functions. The default values can be one or more of the
+ *   following:
+ *   - node: The current template type; for example, "theming hook".
+ *   - node-[type]: The current node type. For example, if the node is a
+ *     "Blog entry" it would result in "node-blog". Note that the machine
+ *     name will often be in a short form of the human readable label.
+ *   - node-teaser: Nodes in teaser form.
+ *   - node-preview: Nodes in preview mode.
+ *   The following are controlled through the node publishing options.
+ *   - node-promoted: Nodes promoted to the front page.
+ *   - node-sticky: Nodes ordered above other non-sticky nodes in teaser
+ *     listings.
+ *   - node-unpublished: Unpublished nodes visible only to administrators.
+ * - $title_prefix (array): An array containing additional output populated by
+ *   modules, intended to be displayed in front of the main title tag that
+ *   appears in the template.
+ * - $title_suffix (array): An array containing additional output populated by
+ *   modules, intended to be displayed after the main title tag that appears in
+ *   the template.
+ *
+ * Other variables:
+ * - $node: Full node object. Contains data that may not be safe.
+ * - $type: Node type; for example, story, page, blog, etc.
+ * - $comment_count: Number of comments attached to the node.
+ * - $uid: User ID of the node author.
+ * - $created: Time the node was published formatted in Unix timestamp.
+ * - $classes_array: Array of html class attribute values. It is flattened
+ *   into a string within the variable $classes.
+ * - $zebra: Outputs either "even" or "odd". Useful for zebra striping in
+ *   teaser listings.
+ * - $id: Position of the node. Increments each time it's output.
+ *
+ * Node status variables:
+ * - $view_mode: View mode; for example, "full", "teaser".
+ * - $teaser: Flag for the teaser state (shortcut for $view_mode == 'teaser').
+ * - $page: Flag for the full page state.
+ * - $promote: Flag for front page promotion state.
+ * - $sticky: Flags for sticky post setting.
+ * - $status: Flag for published status.
+ * - $comment: State of comment settings for the node.
+ * - $readmore: Flags true if the teaser content of the node cannot hold the
+ *   main body content.
+ * - $is_front: Flags true when presented in the front page.
+ * - $logged_in: Flags true when the current user is a logged-in member.
+ * - $is_admin: Flags true when the current user is an administrator.
+ *
+ * Field variables: for each field instance attached to the node a corresponding
+ * variable is defined; for example, $node->body becomes $body. When needing to
+ * access a field's raw values, developers/themers are strongly encouraged to
+ * use these variables. Otherwise they will have to explicitly specify the
+ * desired field language; for example, $node->body['en'], thus overriding any
+ * language negotiation rule that was previously applied.
+ *
+ * @see template_preprocess()
+ * @see template_preprocess_node()
+ * @see template_process()
+ *
+ * @ingroup themeable
+ */
+?>
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+
+  <?php print $user_picture; ?>
+
+  <?php print render($title_prefix); ?>
+  <?php if (!$page): ?>
+    <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
+
+  <?php if ($display_submitted): ?>
+    <div class="submitted">
+      <?php print $submitted; ?>
+    </div>
+  <?php endif; ?>
+  <?php
+      // We hide the comments and links now so that we can render them later.
+      hide($content['comments']);
+      hide($content['links']);
+      $price = $content['product:commerce_price'][0]['#markup'];
+      $imageFile = $content['product:field_dog_picture']['#items'][0]['filename'];
+      $formAction = $content['field_product'][0]['#action'];
+      $formProductId = $content['field_product'][0]['product_id']['#value'];
+      $formID = $content['field_product'][0]['#form_id'];
+      $formBuildId = $content['field_product'][0]['form_build_id']['#value'];
+      //print var_dump($content['field_product'][0]);
+    ?>
+      <div class="row well">
+    	<div class="col-md-6">
+    		<img src="http://localhost/chapman/sites/default/files/styles/medium/public/<?php print render($imageFile); ?>">
+    	</div>
+    	<div class="col-md-6">
+    		<h3><?php print $title; ?></h3>
+    		<h4>By: Q. Ulysses Chapman</h4>
+    		<h4>Price: <?php print render($price); ?></h4>
+    		<form class="commerce-add-to-cart commerce-cart-add-to-cart-form-1" action="<?php print render($formAction); ?>" method="post" id="commerce-cart-add-to-cart-form-1" accept-charset="UTF-8">
+    			<div>
+	    			<input name="product_id" value="<?php print render($formProductId); ?>" type="hidden">
+			<input name="form_build_id" value="<?php print render($formBuildId); ?>" type="hidden">
+			<input name="form_id" value="<?php print render($formID); ?>" type="hidden">
+					<input name="quantity" value="1" type="hidden">
+					<button class="btn btn-success form-submit" id="edit-submit" name="op" value="Add to cart" type="submit">Add to cart</button>
+				</div>
+			</form>
+    	</div>
+    </div>
+  <div class="content"<?php print $content_attributes; ?>>
+    <ul class="nav nav-tabs">
+	  <li class="active"><a data-toggle="tab" href="#overview">Overview</a></li>
+	</ul>
+	<div class="tab-content">
+	  <div id="overview" class="tab-pane fade in active">
+	    <h3>Overview</h3>
+	    <p><?php print render($content['body']['#items'][0]['value'])?></p>
+	  </div>
+	</div>
+  </div>
+
+  <?php print render($content['links']); ?>
+
+  <?php print render($content['comments']); ?>
+
+</div>
