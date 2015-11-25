8516fb26fa82922fe39c471958f8b93ee7086419
mamistry_turner <manish.mistry@turner.com>
2015-11-25 12:15:54 -0500

    Fixing bg fade on audio page

--- sites/all/themes/bootstrap_subtheme/js/jsplayerCustom.js
+++ sites/all/themes/bootstrap_subtheme/js/jsplayerCustom.js
@@ -1,10 +1,7 @@
 jQuery( document ).ready(function() {
 	jQuery('.jp-jplayer').bind(jQuery.jPlayer.event.ready  ,function(event) { 
 		jQuery('.jp-jplayer').bind(jQuery.jPlayer.event.pause  ,function(event) { 
-		    jQuery('body').fadeTo('slow', 0.3, function()
-		    {
-		        jQuery(this).css('background-image', 'url("../sites/all/themes/bootstrap_subtheme/img/dark_earth.jpg")');
-		     }).fadeTo('slow', 1);
+			jQuery('body').css('background-image', 'url("../sites/all/themes/bootstrap_subtheme/img/dark_earth.jpg")');
 		  });
 	  });
 });
