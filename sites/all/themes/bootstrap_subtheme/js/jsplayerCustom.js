e08042aad4b48cca2d3d1d765b512d80bfd9beae
dermaine <dermaine@hotmail.com>
2015-11-25 12:58:46 -0500

    audio player

--- sites/all/themes/bootstrap_subtheme/js/jsplayerCustom.js
+++ sites/all/themes/bootstrap_subtheme/js/jsplayerCustom.js
@@ -3,7 +3,7 @@ jQuery( document ).ready(function() {
 		jQuery('.jp-jplayer').bind(jQuery.jPlayer.event.pause  ,function(event) { 
 		    jQuery('body').fadeTo('slow', 0.3, function()
 		    {
-		        jQuery(this).css('background-image', 'url("../sites/all/themes/bootstrap_subtheme/img/dark_earth.jpg")');
+		        jQuery(this).css('background-image', 'url("../sites/all/themes/bootstrap_subtheme/img/space.gif")');
 		     }).fadeTo('slow', 1);
 		  });
 	  });
