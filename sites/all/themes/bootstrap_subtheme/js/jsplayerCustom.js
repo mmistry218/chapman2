8d826fb983df6b6eacf0385ce00719afc19d3954
mamistry_turner <manish.mistry@turner.com>
2015-11-25 16:24:48 -0500

    Fixing jplayer bg transition

--- sites/all/themes/bootstrap_subtheme/js/jsplayerCustom.js
+++ sites/all/themes/bootstrap_subtheme/js/jsplayerCustom.js
@@ -1,7 +1,7 @@
 jQuery( document ).ready(function() {
 	jQuery('.jp-jplayer').bind(jQuery.jPlayer.event.ready  ,function(event) { 
 		jQuery('.jp-jplayer').bind(jQuery.jPlayer.event.pause  ,function(event) { 
-			jQuery(this).css('background-image', 'url("../sites/all/themes/bootstrap_subtheme/img/space.gif")');
+			jQuery('body').css('background-image', 'url("../sites/all/themes/bootstrap_subtheme/img/space.gif")');
 		  });
 	  });
 });
